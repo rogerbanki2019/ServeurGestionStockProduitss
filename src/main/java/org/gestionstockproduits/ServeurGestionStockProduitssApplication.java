fd141ff0a5fa85d213d51956583c04c855b05eb8
rogerbanki2019 <57193636+rogerbanki2019@users.noreply.github.com>
2019-12-12 16:54:54 +0100

    first commit

---
+++ src/main/java/org/gestionstockproduits/ServeurGestionStockProduitssApplication.java
@@ -0,0 +1,13 @@
+package org.gestionstockproduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduitssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProduitssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gestionstockproduits/ServeurGestionStockProduitssApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gestionstockproduits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
